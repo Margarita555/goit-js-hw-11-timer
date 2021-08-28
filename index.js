578c843dd048ba7925d32752bf2f876ef893e6f6
Margarita555 <a_rita@ukr.net>
2021-08-28 20:27:42 +0300

    +

---
+++ index.js
@@ -0,0 +1,51 @@
+const daysFace = document.querySelector('span[data-value="days"]');
+const hoursFace = document.querySelector('span[data-value="hours"]');
+const minsFace = document.querySelector('span[data-value="mins"]');
+const secsFace = document.querySelector('span[data-value="secs"]');
+
+class CountdownTimer{
+    constructor({ onTick, targetDate,selector }) {
+        this.targetDate = targetDate.getTime();
+        this.intervalId = null;
+        this.onTick = onTick;
+        this.selector = selector;
+    }
+    start() {
+        this.intervalId = setInterval(() => {
+            const currentTime = Date.now();
+            const time = this.targetDate - currentTime;
+            const timeComponents = this.getTimeComponents(time);
+            this.onTick(timeComponents);
+            if (time < 0) {
+    clearInterval(this.intervalId);
+    document.querySelector(this.selector).innerHTML = "EXPIRED";
+  }
+        },1000)
+    }
+   
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        return{days, hours, mins, secs}
+    }
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }   
+}
+
+const newCountDownTimer = new CountdownTimer({
+  onTick: updateClockface,
+  selector: '#timer-1',
+  targetDate: new Date('Sep 17, 2021'),
+});
+
+document.addEventListener("DOMContentLoaded", newCountDownTimer.start.bind(newCountDownTimer));
+
+function updateClockface({ days, hours, mins, secs }) {
+    daysFace.textContent = `${days}`;
+    hoursFace.textContent = `${hours}`;
+    minsFace.textContent = `${mins}`;
+    secsFace.textContent = `${secs}`;
+}
